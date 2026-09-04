952754643fb014298a24d54f292f1c725fc5ebcf
latercomer <latercomer@qq.com>
2026-09-04 19:50:13 +0800

    fix(theme): 产品详情页帮助按钮使用 frontmatter.helpUrl
    ProductLayout 详情页的「帮助文档」按钮之前写死 /manual/，忽略了 frontmatter.helpUrl，导致产品详情页的 helpUrl 不生效。改为读取 frontmatter.helpUrl，未填写时兜底 /manual/，与列表页 CatalogLayout 行为一致。
    
    Signed-off-by: latercomer <latercomer@qq.com>

--- .vitepress/theme/ProductLayout.vue
+++ .vitepress/theme/ProductLayout.vue
@@ -19,6 +19,7 @@ const summary = computed(() => (frontmatter.value.summary || frontmatter.value.d
 
 const price = computed(() => (frontmatter.value.price ?? null) as number | null)
 const shopUrl = computed(() => (frontmatter.value.shopUrl || 'https://shop103678810.taobao.com') as string)
+const helpUrl = computed(() => (frontmatter.value.helpUrl || '/manual/') as string)
 const tags = computed(() => (frontmatter.value.tags || []) as string[])
 
 // 正文 tab 切换：事件委托，处理 markdown 渲染出的 `.heading-tab` 点击
@@ -66,7 +67,7 @@ onBeforeUnmount(() => document.removeEventListener('click', onTabClick))
 
         <div class="actions">
           <a :href="shopUrl" class="btn btn-primary" target="_blank" rel="noopener noreferrer"> 立即购买 </a>
-          <a href="/manual/" class="btn btn-secondary">帮助文档</a>
+          <a :href="helpUrl" class="btn btn-secondary">帮助文档</a>
         </div>
 
         <div v-if="tags.length" class="tags">
